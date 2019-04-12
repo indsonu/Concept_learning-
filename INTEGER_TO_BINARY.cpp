c8cf87635204366e7a1384752883a882748570a5
S$onu <32143858+indsonu@users.noreply.github.com>
2019-04-12 14:40:30 +0530

    Create INTEGER_TO_BINARY.cpp

---
+++ INTEGER_TO_BINARY.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+string result(int t){
+    int p;
+    string s="";
+    while(t!=0){
+        p=t&1;      // MAIN CASE                                            1 0 1
+        s+=to_string(p);                                                    0 0 1
+        t=t>>1;                                                      ANS :  0 0 1
+    }
+    reverse(begin(s),end(s));
+    return s;
+}
+int main(){
+    int t,p;
+    string s;
+    cin>>t;
+    s=result(t);
+    cout<<s;
+}
