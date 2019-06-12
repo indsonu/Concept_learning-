1adeea06d711f4be032fd8b7d210a20527d34bcb
S$onu <32143858+indsonu@users.noreply.github.com>
2019-06-12 11:57:32 +0530

    Update SUBSEQUENCE.cpp

--- SUBSEQUENCE.cpp
+++ SUBSEQUENCE.cpp
@@ -4,8 +4,8 @@ using namespace std;
 void LetsGenerate(string s,int no){
     int i=0;
     while(no>0){
-        (no&1)?cout<<s[i++]:cout<<"";   // BASIC BIT MASKING IS DONE HERE    LAST BIT IS & WITH ALL THE DIGIT OF THE NUMBER AND COMPARED
-        no=no>>1;
+        (no&1)?cout<<s[i]:cout<<"";   // BASIC BIT MASKING IS DONE HERE    LAST BIT IS & WITH ALL THE DIGIT OF THE NUMBER AND COMPARED
+        no=no>>1;i++;
     }
 }
 void GenerateSub(string s){
